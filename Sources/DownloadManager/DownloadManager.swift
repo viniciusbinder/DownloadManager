536b9bddea4514d9b3f17b91d0dcd6902b11c342
Vinícius Binder <vinicius.binder@gmail.com>
2025-04-17 00:41:04 -0300

    Create `DownloadManager` skeleton

--- Sources/DownloadManager/DownloadManager.swift
+++ Sources/DownloadManager/DownloadManager.swift
@@ -1,2 +1,38 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+enum DownloadStatus {
+    case progress(Double)
+    case completed(Data)
+    case failed(Error)
+}
+
+protocol DownloadManager: Sendable {
+    func startDownload(from url: URL) async
+    func cancelDownload(for url: URL) async
+    func status(of url: URL) async -> DownloadStatus?
+    func stream(for url: URL) async -> AsyncStream<DownloadStatus>?
+}
+
+extension DownloadManager where Self == DefaultDownloadManager {
+    static var `default`: DefaultDownloadManager { .init() }
+}
+
+actor DefaultDownloadManager: DownloadManager {
+    func startDownload(from url: URL) async {
+        // TODO: Start download and store result internally
+    }
+
+    func cancelDownload(for url: URL) async {
+        // TODO: Cancel ongoing download
+    }
+
+    func status(of url: URL) async -> DownloadStatus? {
+        // TODO: Return current download status
+        return nil
+    }
+
+    func stream(for url: URL) async -> AsyncStream<DownloadStatus>? {
+        // TODO: Publish ongoing progress updates
+        return nil
+    }
+}
